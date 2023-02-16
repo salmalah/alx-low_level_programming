b766b103f0a855db9d3aa6973ff9707e796f89a8
salmalah <s.salmalahmidi@gmail.com>
2023-02-16 10:26:27 -0800

    C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - This program print a messag
- * Return Always zero (Success)
+ * Return Always 0 (Success)
  */
 
 int main(void)
@@ -10,4 +10,5 @@ int main(void)
 
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
