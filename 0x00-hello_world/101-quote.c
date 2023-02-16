2cf83eee7468469193b40bdf187203a3cfee2880
salmalah <s.salmalahmidi@gmail.com>
2023-02-16 12:37:09 -0800

    c program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Projet c
+ *
+ * Description : this program print a msg without using puts or printf
+ *
+ * Return 0 (Seccuss)
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
+	return (1);
+}
