b4346253942bddb8bd22fa5fa705a86a0f705c12
salmalah <s.salmalahmidi@gmail.com>
2023-02-16 10:11:39 -0800

    C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point 
  * Return Always 0 (Success)
@@ -6,6 +6,6 @@
 
 int main(void)
 {
-	printf(""Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
