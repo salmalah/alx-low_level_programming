a1b147322479a3385387e2d693514d36dff5a9b7
salmalah <s.salmalahmidi@gmail.com>
2023-02-16 11:19:06 -0800

    size  of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
 	printf("Size of a char:", sizeof(char));
 	printf("Size of an int:", sizeof(int));
 	printf("Size of a long int:", sizeof(long int));
+	printf("Size of a long long int:", sizeof(long long int));
 	printf("Size of a float:", sizeof(float));
 	return (0);
 }
