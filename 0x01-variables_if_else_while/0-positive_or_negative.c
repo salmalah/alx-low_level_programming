45de9c7d8f9fdaf4cd24aec0003449261ac51557
salmalah <s.salmalahmidi@gmail.com>
2023-02-17 13:17:27 -0800

    program  assign a random number to the variable n

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : code c
+ *
+ * Return : Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
