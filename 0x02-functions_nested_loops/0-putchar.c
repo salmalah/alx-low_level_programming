fdad97519d75799405d1c0c01649131bee2255b7
salmalah <s.salmalahmidi@gmail.com>
2023-02-21 07:41:37 -0800

    program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,11 +8,13 @@
  */
 int main(void)
 {
-	int word[8]={95,112,117,116,99,104,97,114};
+	int word[8] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int i;
 
-	 for (i = 0; i <= 8; i++)
-		 _putchar(word[i]);
-	 _putchar('\n');
-	 return (0);
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
