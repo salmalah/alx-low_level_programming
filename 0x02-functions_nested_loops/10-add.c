e8141d41f276a2fea6e28d1d9b19ce342fff17f2
salmalah <s.salmalahmidi@gmail.com>
2023-02-21 13:05:24 -0800

    m

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * @x: input number
  * @y: input number
  * Descripton: It a function that adds two integers and returns the result.
- * Return : 0 if seccussful else 1
+ * Return: 0 if seccussful else 1
  */
 int add(int x, int y)
 {
