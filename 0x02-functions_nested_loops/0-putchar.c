9aba722771fb2762ab1b1e8a59498a5c7368eb95
salmalah <s.salmalahmidi@gmail.com>
2023-02-21 07:32:27 -0800

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - This is the starting of the program
+ *
+ * Description : prints putchar fonction
+ *
+ *Return: 0 if successfull else 1
+ */
+int main(void)
+{
+	int word[8]={95,112,117,116,99,104,97,114};
+	int i;
+
+	 for (i = 0; i <= 8; i++)
+		 _putchar(word[i]);
+	 _putchar('\n');
+	 return (0);
+}
