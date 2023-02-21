9b1d6c58f5dbc1f6b465ecac6d407d52bee764c6
salmalah <s.salmalahmidi@gmail.com>
2023-02-21 14:21:02 -0800

    m

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,8 +30,8 @@ void print_times_table(int n)
 				{
 					_putchar((prod / 100) + 48);
 					_putchar((prod / 10) % 10 + 48);
-				
-				}else if (prod <= 99 && prod >= 10)
+				}
+				else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
 				_putchar((prod % 10) + 48);
 			}
