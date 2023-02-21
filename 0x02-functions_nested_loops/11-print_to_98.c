5e054747c657e8652083c7048604f58a44e9c729
salmalah <s.salmalahmidi@gmail.com>
2023-02-21 14:04:40 -0800

    m

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,9 @@
  */
 void print_to_98(int n)
 {
-	
 	if (n <= 98)
 	{
-		for(; n <= 98; n++)
+		for (; n <= 98; n++)
 		{
 			printf("%d", n);
 			if (n == 98)
@@ -22,7 +21,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for(; n >= 98; n--)
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
 			if (n == 98)
