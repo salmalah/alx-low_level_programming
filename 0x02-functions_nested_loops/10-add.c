e385e8bdd28904740bfdf81c682f1ed815879f97
salmalah <s.salmalahmidi@gmail.com>
2023-02-21 12:59:41 -0800

    m

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Check description
+ * Descripton: It a function that adds two integers and returns the result.
+ * Return : 0 if seccussful else 1
+ */
+int add(int x, int y)
+{
+	int r;
+
+	r = x + y;
+	_putchar(r);
+	_putchar('\n');
+	return (0);
+}
