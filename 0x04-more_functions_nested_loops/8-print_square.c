e7234e666b6d666ddbefddacfdcf5241727d19bb
salmalah <s.salmalahmidi@gmail.com>
2023-02-23 05:23:12 -0800

    Test0

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_square - Check description
+ * Description: It a function that prints a square, followed by a new lin
+ * @size: input number
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int i, j, s;
+
+	i = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < size)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
