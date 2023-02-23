651a1e59bf69d6ff0afa7de7ee4c5716628e5c38
salmalah <s.salmalahmidi@gmail.com>
2023-02-23 04:52:33 -0800

    Yes

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - Check description
+ * Description: It a function that prints 10 times the numbers, from 0 to 14
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+		{
+			if (j <= 9)
+				_putchar(' ');
+			else
+			{
+				_putchar (' ');
+				_putchar((j / 10) + 48);
+			}
+			_putchar((j % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
