9a4c008df942a3a67b775fb9772826690af822fd
salmalah <s.salmalahmidi@gmail.com>
2023-02-23 04:26:15 -0800

    Ok

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mult - Check description
+ * Description: It a function that multiplies two integers.
+ * @a : input number
+ * @b : input number
+ * Return: 0 if seccussful else 1
+ */
+int mult(int a, int b)
+{
+	return (a * b);
+}
