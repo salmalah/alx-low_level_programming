2ba3fa02b3475ae12beecbedff2dcf708b888438
salmalah <s.salmalahmidi@gmail.com>
2023-02-23 04:46:07 -0800

    m

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
-void print_nost_numbers(void);
+void print_most_numbers(void);
 void print_numbers(void);
 void print_diagonal(int n);
 void print_square(int size);
