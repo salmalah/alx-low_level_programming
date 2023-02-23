a9e3cd19ebd4371be4d7dd23c154ee3a46fb81d6
salmalah <s.salmalahmidi@gmail.com>
2023-02-23 06:12:35 -0800

    END

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_number - Check description
+ * Description: It a function that prints an integer
+ * @n: input integer
+ * Return: 0 Always
+ */
+void print_number(int n)
+{
+	_putchar(n + '0');
+}
