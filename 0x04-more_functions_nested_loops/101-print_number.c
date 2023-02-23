39bf0668a68f04fd805bda0a94a69cbd468af58c
salmalah <s.salmalahmidi@gmail.com>
2023-02-23 06:15:11 -0800

    END

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,5 +7,13 @@
  */
 void print_number(int n)
 {
-	_putchar(n + '0');
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar(n + '0');
+	}
+	else
+		_putchar(n + '0');
 }
