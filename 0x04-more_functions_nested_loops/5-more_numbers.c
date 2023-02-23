540ce62ba5ce03ac63ba6dfbb414f1cebed88ec0
salmalah <s.salmalahmidi@gmail.com>
2023-02-23 04:57:34 -0800

    Yes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,8 @@ void more_numbers(void)
 	{
 		for (j = 0 ; j <= 14 ; j++)
 		{
-			if (j <= 9)
-				_putchar(' ');
-			else
-			{
-				_putchar (' ');
+			if (j > 9)
 				_putchar((j / 10) + 48);
-			}
 			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
