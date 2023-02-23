06df018fde5b72f368903310a8b2b1064bd579dd
salmalah <s.salmalahmidi@gmail.com>
2023-02-23 04:29:56 -0800

    Ok

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * Description: It a function that multiplies two integers.
  * @a : input number
  * @b : input number
- * Return: 0 if seccussful else 1
+ * Return: Always 0.
  */
 int mul(int a, int b)
 {
