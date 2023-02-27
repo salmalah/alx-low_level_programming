07b8dd9cff8ddf649b0ccf8075689d9e3a2ccc93
salmalah <s.salmalahmidi@gmail.com>
2023-02-27 11:25:13 -0800

    unction that copies the string pointed to by src

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - a function that copies the string pointer
+ * @dest: A pointer to destination of string
+ * @src: A pointer to source string to copy from
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+}
