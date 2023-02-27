f6a4c4c4ca9f873688244715b41bca32fd682d7f
salmalah <s.salmalahmidi@gmail.com>
2023-02-27 12:01:49 -0800

    Advanced

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _atoi - a function that converts string to integer
+ * @s: An input string
+ * Return: integer from conversion
+ */
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int n = strlen(s);
+
+	for (int i = 0; i < n; i++)
+	{
+		num = num * 10 + (s[i] - 48);
+	}
+	return (num);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,3 +9,4 @@ void puts_half(char *str);
 void _puts(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
