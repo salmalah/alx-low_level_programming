511b55c332e6e5f8a0e98fa1d644b641d5324938
salmalah <s.salmalahmidi@gmail.com>
2023-03-01 10:52:52 -0800

    test

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *s1, char *s2)
 	if (strlen(s1) == strlen(s2))
 		return (0);
 	else if (strlen(s1) > strlen(s2))
-		return (-15);
-	else
 		return (15);
+	else
+		return (-15);
 }
