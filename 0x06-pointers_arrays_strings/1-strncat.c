9f65d04740a05782b5e12d7c64d6bdf14d800e26
salmalah <s.salmalahmidi@gmail.com>
2023-03-01 09:22:38 -0800

    don't think

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncat - a function that concatenates two strings
+ * @dest: an input string
+ * @src: an input string
+ * @n: This is the maximum number of characters to be appended
+ * Return: A pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	dest = strncat(dest, src, n);
+	return (dest);
+}	
