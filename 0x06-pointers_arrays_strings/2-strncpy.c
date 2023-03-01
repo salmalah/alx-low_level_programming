9a11b82560c5c4d94481dbd4cefe9167712d183f
salmalah <s.salmalahmidi@gmail.com>
2023-03-01 10:25:23 -0800

    a function that copys the strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <string.h>
 /**
- * *_strncat - a function that copy a strings
+ * *_strncpy - a function that copy a strings
  * @dest: an input string
  * @src: an input string
  * @n: This is the maximum number of characters to be appended
  * Return: A pointer to the resulting string
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
