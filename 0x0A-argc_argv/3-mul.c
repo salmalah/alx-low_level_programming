0e8b9f6242344ca48acb630a24ad5c65c38d8d85
salmalah <s.salmalahmidi@gmail.com>
2023-03-10 08:24:41 -0800

    TEST3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,14 +7,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int N1 , N2, result;
+	int result;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	result = argv[1] * argv[2];
+	result = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", result);
 	return (0);
 }
