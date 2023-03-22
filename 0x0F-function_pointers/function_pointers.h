5c9a429c08ed3a5316a775e3cdafe8d281e4d3f1
salmalah <s.salmalahmidi@gmail.com>
2023-03-22 08:57:29 -0700

    header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef _FUNCTION_POINTERS__H_
-#define _FUNCTION_POINTERS__H_
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H_
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
