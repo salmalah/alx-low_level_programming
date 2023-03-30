c3dd1a17703d261b81ca24207b56a7b4e11799a7
salmalah <s.salmalahmidi@gmail.com>
2023-03-30 08:41:08 -0700

    fix header file error

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,4 +24,4 @@ size_t list_len_recursion(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-#endif 
+#endif
