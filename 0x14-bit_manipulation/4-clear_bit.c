669ecffbb88c98c89bfa43626b31180dd419de4b
salmalah <s.salmalahmidi@gmail.com>
2023-04-08 11:46:10 -0700

    fix error task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,9 +10,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int T;
 
+	T = ~(1 << index);
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
-	T = ~(1 << index);
 	*n &= T;
 	return (1);
 }
