525ac43ff4003e0fe683c4d355f2ec7e96dc076f
salmalah <s.salmalahmidi@gmail.com>
2023-04-08 11:52:52 -0700

    test

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits 
+ * @n: input number
+ * @m: second input number
+ * Return: number of 
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int dor = n ^ m, check_number = 1;
+	unsigned int count = 0, i;
+
+	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	{
+		if (check_number == (dor & check_number))
+			count++;
+		check_number <<= 1;
+	}
+	return (count);
+}
