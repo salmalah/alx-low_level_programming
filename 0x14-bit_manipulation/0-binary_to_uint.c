ca66f931b5a15e9f08afe8824b56cfe44d1dbcbb
salmalah <s.salmalahmidi@gmail.com>
2023-04-08 09:24:17 -0700

    try to fix error

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int k = 1, f = 0, length;
-        int l;
+	unsigned int k = 1, f = 0, length;
+	int l;
 
-        length = strlen(b);
-        for (l = length - 1; l >= 0; l--)
-        {
-                if (b[l] != '0' && b[l] != '1')
-                        return (0);
-                if (b[l] == '1')
-                {
-                        f += k;
-                }
-                k *= 2;
-        }
-        return (f);
+	length = strlen(b);
+	for (l = length - 1; l >= 0; l--)
+	{
+		if (b[l] != '0' && b[l] != '1')
+			return (0);
+		if (b[l] == '1')
+			f += k;
+		k *= 2;
+	}
+	return (f);
 }
