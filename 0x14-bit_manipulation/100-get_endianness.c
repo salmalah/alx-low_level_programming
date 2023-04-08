39de7072b089a583c2587eeaff86b7f0d8641cf4
salmalah <s.salmalahmidi@gmail.com>
2023-04-08 14:22:46 -0700

    first test advaned task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int x = 1;
+	char *b_ptr = (char*)&x;
+
+	if (*b_ptr == 1)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
