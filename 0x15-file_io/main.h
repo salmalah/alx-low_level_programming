e6483de409eed0bfc90f3b1f48d5e0bea88fe444
salmalah <s.salmalahmidi@gmail.com>
2023-04-09 10:38:32 -0700

    test

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,5 @@
 #define _MAIN_H_
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-
-
+int append_text_to_file(const char *filename, char *text_content);
 #endif
