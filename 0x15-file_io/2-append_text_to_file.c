4f825be5f29ace7363ccba71b91a673db25a3da8
salmalah <s.salmalahmidi@gmail.com>
2023-04-09 10:42:04 -0700

    change code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	flo = open(filename, O_RDWR | O_APPEND);
-	if (fdlo < 0)
+	if (flo < 0)
 		return (-1);
 	if (text_content == NULL)
 	{
