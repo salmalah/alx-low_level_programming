5b0cfd6afeed7f34a86a2d2d4afee30ac1a88292
salmalah <s.salmalahmidi@gmail.com>
2023-05-18 08:46:56 -0700

    0. Print list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,5 +6,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+	const dlistint_t *new_l = h;
+	int count = 0;
 
+	while (new_l)
+	{
+		printf("%d\n", new_l->n);
+		new_l = new_l->next;
+		count++;
+	}
+	return (count);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
