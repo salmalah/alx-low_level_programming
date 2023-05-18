191d0d4ad06dd57928b115c7a7c2dc5ab1c8d3eb
salmalah <s.salmalahmidi@gmail.com>
2023-05-18 10:57:08 -0700

    6. Sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,4 +1,9 @@
 #include "lists.h"
+/**
+ * sum_dlistint - A function that print sum of list elements
+ * @head: The pointer to head of the list
+ * Return: the sum
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
