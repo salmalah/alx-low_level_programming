3b2bb53697f2b56bd73a15bd3da3f1824158a37c
salmalah <s.salmalahmidi@gmail.com>
2023-06-08 11:58:36 -0700

    2. key -> index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,7 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash_v;
+	unsigned long int hash_val;
 
 	hash_val = hash_djb2(key);
 	return (hash_val % size);
