e0bb066e1479555965540256bbefcac39d284790
salmalah <s.salmalahmidi@gmail.com>
2023-06-08 12:24:23 -0700

    5. >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - a function that prints a hash table
+ * @ht: is the hash table
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *n;
+	unsigned long int j;
+	int num = 1;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (j = 0; j < ht->size; j++)
+	{
+		n = ht->array[j];
+		while (n != NULL)
+		{
+			if (num)
+				num = 0;
+			else
+				printf(", ");
+			printf("'%s': '%s'", n->key, n->value);
+			n = n->next;
+		}
+
+		
+	}
+	printf("}\n");
+}
