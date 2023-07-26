896d81e51ecbb10a069735a206b37d297210d55a
salmalah <s.salmalahmidi@gmail.com>
2023-07-26 11:06:36 -0700

    5. Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module defines the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    Initialise functions
+    """
+    per = 0
+    gg = len(grid[0])
+    g = len(grid)
+    for r in range(g):
+        for c in range(gg):
+            if grid[r][c] == 1:
+                per += 4
+                if r > 0 and grid[r - 1][c] == 1:
+                    per -= 2
+                if grid[r][c - 1]  == 1 and c > 0:
+                    per -= 2
+    return per
